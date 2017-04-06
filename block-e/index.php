4e2b5efb0d742f3744a2d9a9d41a0bbf64ae3fa5
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-04-06 23:44:49 +0700

    Update

--- block-e/index.php
+++ block-e/index.php
@@ -1,10 +1,12 @@
 <?php
 
-Block::set('e', function($content) {
+function fn_block_replace_e($content) {
     return Block::replace('e', function($content) {
         ob_start();
         extract(Lot::get(null, []));
         eval($content);
         return ob_get_clean();
     }, $content);
-});
+}
+
+Block::set('e', 'fn_block_replace_e', 9);
