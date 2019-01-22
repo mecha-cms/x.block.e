2d2219151c2fc8d96677ba3396d042c91cb87f7b
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-01-22 18:17:39 +0700

    Micro Optimization

--- block.e/index.php
+++ block.e/index.php
@@ -3,12 +3,12 @@
 function e($content, array $lot = []) {
     $that = $this;
     return $content ? \Block::replace('e', function($content) use($lot, $that) {
-        ob_start();
+        \ob_start();
         \fn(function() use($content) {
-            extract(\Lot::get(), EXTR_SKIP);
+            extract(\Lot::get(), \EXTR_SKIP);
             eval($content);
         }, $lot, $that, \Page::class);
-        return ob_get_clean();
+        return \ob_get_clean();
     }, $content) : $content;
 }
 
