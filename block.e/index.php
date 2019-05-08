669ba0031e95bbb25ab985616107674f771dc4ea
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-05-08 11:02:44 +0700

    #54

--- block.e/index.php
+++ block.e/index.php
@@ -1,11 +1,11 @@
-<?php namespace fn\block;
+<?php namespace _\block;
 
 function e($content, array $lot = []) {
     $that = $this;
     return $content ? \Block::replace('e', function($content) use($lot, $that) {
         \ob_start();
-        \fn(function() use($content) {
-            extract(\Lot::get(), \EXTR_SKIP);
+        \fire(function() use($content) {
+            extract($GLOBALS, \EXTR_SKIP);
             eval($content);
         }, $lot, $that, \Page::class);
         return \ob_get_clean();
