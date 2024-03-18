fac673f2d414194c7530d987bc163797b60b894a
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-03-18 12:44:37 +0700

    Use `lot()` Function

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ function block__e(string $content, array $lot = []) {
     if (isset($lot[1]) && \is_string($lot[1])) {
         \ob_start();
         \fire(function () use ($lot) {
-            \extract($GLOBALS, \EXTR_SKIP);
+            \extract(\lot(), \EXTR_SKIP);
             eval($lot[1]);
         }, [], $this);
         return \ob_get_clean();
